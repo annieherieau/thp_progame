cc9495bb7cdef88868d825e3a91035271ef143a4
annieherieau <annie.herieau@gmail.com>
2024-04-29 11:42:36 +0200

    template pageList

--- src/js/PageList.js
+++ src/js/PageList.js
@@ -1,3 +1,45 @@
-export const PageList = (argument = '') => {
-  console.log('Page List', argument);
+export const PageList = (argument = "") => {
+  console.log("Page List", argument);
+
+  // Préparer le template
+  const preparePage = () => {
+    const cleanedArgument = argument.trim().replace(/\s+/g, '-');
+
+    // affichage des articles
+    const displayResults = (articles) => {
+      const resultsContent = articles.map((article) => (
+        `<article class="cardGame">
+          <h1>${article.name}</h1>
+          <h2>${article.released}</h2>
+          <a href="#pagedetail/${article.id}">${article.id}</a>
+        </article>`
+      ));
+      const resultsContainer = document.querySelector('.page-list .articles');
+      resultsContainer.innerHTML = resultsContent.join("\n");
+    };
+
+    // REQUEST
+    const fetchList = (url, argument) => {
+      const finalURL = argument ? `${url}&search=${argument}` : url;
+      fetch(finalURL)
+        .then((response) => response.json())
+        .then((responseData) => {
+          displayResults(responseData.results)
+        });
+    };
+
+    fetchList(`https://api.rawg.io/api/developers?key=${process.env.RAWG_APIKEY}`, cleanedArgument);
+  };
+
+  // Affichage de la page
+  const render = () => {
+    pageContent.innerHTML = `
+      <section class="page-list">
+        <div class="articles">Hey, this page is a PageList template, about : ${argument}</div>
+      </section>
+    `;
+
+    preparePage();
+  };
+  render();
 };
