46ad011693ecd00bf48a0bde1bebcb82c8fbf1b1
annieherieau <annie.herieau@gmail.com>
2024-05-01 00:37:20 +0200

    infos

--- src/js/PageDetail.js
+++ src/js/PageDetail.js
@@ -23,7 +23,7 @@ export const PageDetail = (argument = "") => {
       } = gameData;
 
       let websiteBtn = linkButton(
-        "check Website",
+        "Check Website",
         "btn",
         "websiteBtn",
         website
@@ -42,22 +42,28 @@ export const PageDetail = (argument = "") => {
       artRating.innerText = `${rating.toString()}/5 - ${reviews_count} votes`;
       artDescription.innerHTML = description
         .replace("<p>Plot</p>", "<h5>Plot</h4>")
-        .replace("<p>Gameplay</p>", "<h5>Gameplay</h4>").replace("h3>", "h5>");
-      stores.forEach((s) => {
-        artStores.innerHTML += `<p><a href="${
-          window.location.origin
-        }/#pagelist/${s.store.slug}?type=stores"><strong></strong>${
-          s.store.name
-        } <img class='logo' src="${getLogo(s.store.name)}"></a></p>`;
-      });
-      // fetchData(id, "stores");
+        .replace("<p>Gameplay</p>", "<h5>Gameplay</h4>")
+        .replace("h3>", "h5>");
+
+      displayInfos("Developper", developers);
+      displayInfos("Plaforms", parent_platforms);
+      displayInfos("Publisher", publishers);
+      displayInfos("Genre", genres);
+      displayInfos("Tags", tags);
+      displayStores(id, stores);
       fetchData(id, "screenshots");
       // fetchData(id, "youtube"); // business and entreprise account only
     };
 
-    const fetchGame = (argument) => {
+    function fetchGame(fullargument) {
+      fullargument = fullargument.split("?type=");
+      let argument = fullargument.shift();
+      let type = fullargument.toString();
+
       fetch(
-        `${process.env.RAWG_URL}/games/${argument}?key=${process.env.RAWG_APIKEY}`
+        `${process.env.RAWG_URL}/${type ? type : "games"}/${argument}?key=${
+          process.env.RAWG_APIKEY
+        }`
       )
         .then((response) => response.json())
         .then((responseData) => {
@@ -66,54 +72,83 @@ export const PageDetail = (argument = "") => {
         .catch((error) => {
           console.log(error.message);
         });
-    };
-
-    const fetchData = (gameId, data) => {
-      fetch(
-        `${process.env.RAWG_URL}/games/${gameId}/${data}?key=${process.env.RAWG_APIKEY}`
-      )
-        .then((response) => response.json())
-        .then((responseData) => {
-          displayData(responseData.results, data);
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    };
-
-    function displayData(responseData, data) {
-      switch (data) {
-        case "screenshots":
-          responseData.forEach((shot) => {
-            artScreenshots.innerHTML += `<img class="screenshot" src="${shot.image}">`;
-          });
-          break;
-        case "youtube":
-          responseData.forEach((video) => {
-            console.log(video);
-            // artYoutubeVideos.innerHTML += `<img class="screenshot" src="${shot.image}">`
-          });
-          break;
-        default:
-          break;
-      }
     }
 
     fetchGame(cleanedArgument);
   };
 
+  function fetchData(gameId, data) {
+    fetch(
+      `${process.env.RAWG_URL}/games/${gameId}/${data}?key=${process.env.RAWG_APIKEY}`
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        displayData(responseData.results, data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
+  function displayData(responseData, data) {
+    switch (data) {
+      case "screenshots":
+        responseData.forEach((shot) => {
+          artScreenshots.innerHTML += `<img class="screenshot" src="${shot.image}">`;
+        });
+        break;
+      case "stores":
+        let stores = {};
+        for (const store of responseData) {
+          stores[store.store_id] = store.url;
+        }
+        let storesLinks = document.querySelectorAll(".storesLink");
+        storesLinks.forEach((link) => {
+          link.href = stores[link.id];
+        });
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  function displayStores(gameId, stores) {
+    stores.forEach((s) => {
+      artStores.innerHTML += `<p><a href="" class="storesLink" id="${
+        s.store.id
+      }" target="_blank"><strong>${
+        s.store.name
+      }</strong> <img class='logo' src="${getLogo(s.store.name)}"></a></p>`;
+    });
+    fetchData(gameId, "stores");
+  }
+
+  function displayInfos(title, infos) {
+    let infos_string = infos
+      .map((i) => {
+        if (i.name) {
+          return i.name;
+        } else {
+          return i.platform["name"];
+        }
+      })
+      .join(", ");
+    artInfos.innerHTML += `<div style="min-width:7rem"><h6>${title}</h6><p>${infos_string}</p></div>`;
+  }
+
   const render = () => {
     pageContent.innerHTML = `
       <section class="page-detail">
         <div id="article">
-          <div id="artTitle"><h1 id="artH1"></h1><h4 id="artRating"></h4></div>
-          <div id="artDescription"></d>
-          <div id="artInfos"></div>
+          <div class="d-flex justify-content-between"><h1 id="artH1"></h1><h4 id="artRating" class="text-primary"></h4></div>
+          <div id="artDescription"></div>
+          <div id="artInfos" class="d-flex flex-wrap gap-3"></div>
         </div>
-        <div id="artStores"><h1>BUY</h1></div>
+        <div id="artStores"><h1 class="text-primary">BUY</h1></div>
         <div id="artTrailer"></div>
         <div>
-        <h1>SCREENSHOTS</h1>
+        <h1 class="text-primary">SCREENSHOTS</h1>
         <div id="artScreenshots"></div>
         </div>
       </section>
